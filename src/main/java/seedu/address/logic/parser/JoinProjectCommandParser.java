1f2c624400bc39a945b32710ba7e7db60aba7603
isaactoh <117002710+isaactoh@users.noreply.github.com>
2025-10-23 23:22:23 +0800

    Refactor JoinProjectCommandParser to validate prefixes

--- src/main/java/seedu/address/logic/parser/JoinProjectCommandParser.java
+++ src/main/java/seedu/address/logic/parser/JoinProjectCommandParser.java
@@ -1,11 +1,12 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_MEMBER;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.JoinProjectCommand;
@@ -26,6 +27,11 @@ public class JoinProjectCommandParser implements Parser<JoinProjectCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_MEMBER);
 
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_MEMBER)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, JoinProjectCommand.MESSAGE_USAGE));
+        }
+
         String projectName = argMultimap.getValue(PREFIX_NAME).get().trim();
         if (projectName.isEmpty()) {
             throw new ParseException("Missing project name.\n" + JoinProjectCommand.MESSAGE_USAGE);
@@ -43,5 +49,13 @@ public class JoinProjectCommandParser implements Parser<JoinProjectCommand> {
         }
 
         return new JoinProjectCommand(projectName, memberIndexes);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
