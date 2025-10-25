a540d46ce8f121dd95d08256da0dd1dfb32cceab
isaactoh <isaactoh29@gmail.com>
2025-10-25 13:20:41 +0800

    Add JoinProjectCommand without previous Mistakes

---
+++ src/main/java/seedu/address/logic/parser/JoinProjectCommandParser.java
@@ -0,0 +1,62 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEMBER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.JoinProjectCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new JoinProjectCommand object
+ */
+public class JoinProjectCommandParser implements Parser<JoinProjectCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the JoinProjectCommand
+     * and returns an JoinProjectCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public JoinProjectCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_MEMBER);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_MEMBER)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, JoinProjectCommand.MESSAGE_USAGE));
+        }
+
+        String projectName = argMultimap.getValue(PREFIX_NAME).get().trim();
+        if (projectName.isEmpty()) {
+            throw new ParseException("Missing project name.\n" + JoinProjectCommand.MESSAGE_USAGE);
+        }
+
+        List<String> memberStrings = argMultimap.getAllValues(PREFIX_MEMBER);
+        if (memberStrings.isEmpty()) {
+            throw new ParseException("At least one member index must be provided.\n"
+                    + JoinProjectCommand.MESSAGE_USAGE);
+        }
+
+        List<Index> memberIndexes = new ArrayList<>();
+        for (String s : memberStrings) {
+            memberIndexes.add(ParserUtil.parseIndex(s));
+        }
+
+        return new JoinProjectCommand(projectName, memberIndexes);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values
+     * in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
