b886bac7b2b27a036786ca65e3c521309f7dadac
isaactoh <117002710+isaactoh@users.noreply.github.com>
2025-10-29 17:43:40 +0800

    Modify getProjectByName for case-insensitive search
    Updated getProjectByName method to use case-insensitive comparison.

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -127,14 +127,6 @@ public class ModelManager implements Model {
     }
 
     @Override
-    public Project getProjectByName(String projectName) {
-        requireNonNull(projectName);
-        return addressBook.getProjectList().stream()
-                .filter(p -> p.getName().equals(projectName))
-                .findFirst().orElse(null);
-    }
-
-    @Override
     public boolean hasProject(Project project) {
         requireNonNull(project);
         return addressBook.hasProject(project);
@@ -155,7 +147,7 @@ public class ModelManager implements Model {
     public Project getProjectByName(String projectName) {
         requireNonNull(projectName);
         return addressBook.getProjectList().stream()
-                .filter(p -> p.getName().equals(projectName))
+                .filter(p -> p.getName().equalsIgnoreCase(projectName))
                 .findFirst().orElse(null);
     }
 
