62ff2b5de0a395c95aeebbcde2efaa20b9541792
coolprogrammer000 <54386201+coolprogrammer000@users.noreply.github.com>
2025-11-03 15:51:58 +0800

    Remove unused imports

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DISCORD;
